267e4364df906d4e8c670b342a1e5d3eaef52417
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-05-22 11:10:43 +0530

    AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Models\Admin;
 
 class AdminController extends Controller
 {
     function login(Request $request) {
         //return "admin login";
-        return $request->input();
+
+        //validation
+        $validation = $request->validate([
+            "name" => "required",
+            
+            "password" => "required"
+        ]);
+        $admin = Admin::where([
+            'name' => $request->name,
+            'password' => $request->password
+        ])->first();
+        //return $admin;
+        if(!$admin) {
+            $validation = $request->validate([
+                "user" => "required",
+                            
+            ],[
+                'user.required' => 'Please enter correct username',
+                
+            ]);
+        }
+        
+        //return view('admin', ['name' => $admin->name]);
+        //return $admin->name;
+        Session::put('admin', $admin);
+        return redirect('/dashboard');
+    }
+
+    function dashboard() {
+       // return Session::get('admin');
+        $admin = Session::get('admin');
+        //return view('admin', ['name' => 'Admin']);
+        //return view('admin', ['name' => $admin->name]);
+        if($admin) {
+            return view('admin', ['name' => $admin->name]);
+        }
+        else {
+            return redirect('/admin-login');
+        }
     }
 }
